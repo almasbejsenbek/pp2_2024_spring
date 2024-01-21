74d946441e5671fca51582bf2ee8df75e041a100
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-22 00:01:02 +0600

    Number twenty five commit

---
+++ Week1/Python_Data_Types/5.py
@@ -0,0 +1,4 @@
+x = ("apple", "banana", "cherry")
+print(type(x))
+# tuple type of Data
+#Tuples (type tuple) is an immutable data type in Python that is used to store an ordered sequence of elements.
---
+++ Week1/Python_Data_Types/6.py
@@ -0,0 +1,6 @@
+x = {"name" : "John", "age" : 36}
+print(type(x))
+# dict type of Data 
+# A data structure that allows identifying its elements not by a numeric index but 
+# by an arbitrary one is called a dictionary or an associative array. 
+# The corresponding data structure in the Python language is called dict .
