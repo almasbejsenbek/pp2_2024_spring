64576cc79b5fb7b73bcc3842368bdb8bde10e97f
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-21 23:29:08 +0600

    Number eightteen commit

---
+++ Week1/Python_Variables/6.py
@@ -0,0 +1,7 @@
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+# You can also use the + operator to output multiple variables:
+# Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+print(x + y + z)
