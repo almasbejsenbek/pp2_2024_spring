58d77d38f7419fcd82b0e983652d57d867a9eca8
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-22 00:36:33 +0600

    thirty seven commit

---
+++ Week1/Python_Strings/1.py
@@ -0,0 +1,14 @@
+# You can use three double quotes:
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# Or three single quotes:
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
