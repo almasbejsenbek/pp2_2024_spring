047904a6abf365df1f11af766e832ff0c1a01b0a
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-22 00:23:04 +0600

    thirty three commit

---
+++ Week1/Python_Numbers/6.py
@@ -0,0 +1,6 @@
+import random
+
+print(random.randrange(1, 10000000))
+
+# Python does not have a random() function to make a random number, 
+#but Python has a built-in module called random that can be used to make random numbers:
