1fe6dfeb987d649f4fac0d92f22be0836a4c7752
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-22 00:40:39 +0600

    thirty eight commit

---
+++ Week1/Python_Strings/2.py
@@ -0,0 +1,13 @@
+'''Like many other popular programming languages,
+ strings in Python are arrays of bytes representing unicode characters.
+However, Python does not have a character data type, 
+a single character is simply a string with a length of 1.
+Square brackets can be used to access elements of the string.
+'''
+a = "Almas, Student!"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
