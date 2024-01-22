1e2357069ac0c3fcb839acab55170a5996ea2192
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-22 12:10:43 +0600

    forty four commit

---
+++ Week1/Python_Strings/8.py
@@ -0,0 +1,17 @@
+txt = "Almas Student of Kbtu"
+print(len(txt))
+x = txt[0]
+print(x)
+y = txt[0:5]
+print(y)
+r = txt.strip()
+print(r)
+txt = txt.upper()
+print(txt)
+txt = txt.lower()
+print(txt)
+txt = txt.replace("a","h")
+print(txt)
+age = 18
+txt = "My name is Almas, and I am {}"
+print(txt.format(age))
