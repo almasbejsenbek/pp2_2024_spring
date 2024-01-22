f8257dd6ddae8defd5a20c2a4771891a892a4f64
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-22 12:17:45 +0600

    forty five commit

---
+++ Week1/Python_Home/3.py
@@ -0,0 +1,7 @@
+if 111 > 101:
+    print("Python is a popular programming language.Python can be used on a server to create web applications.")
+if 23 > 23:
+    x = "Almas Krasavchik"
+else: 
+    x ="Almas Student"
+print(x)
