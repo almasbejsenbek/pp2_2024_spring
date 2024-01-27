39a873639749562d8841145d8446f823ccf91644
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-27 17:17:11 +0600

    Week2 8Commit

--- Week2/Python_Operators/3.py
+++ Week2/Python_Operators/3.py
@@ -2,8 +2,8 @@
 # Comparison operators are used to compare two values:
 # Operator	Name	                    Example
 # ==	        Equal	                    x == y	
-# !=	        Not equal	                    x != y	
+# !=	        Not equal	            x != y	
 # >	        Greater than	            x > y	
 # <	        Less than	            x < y	
-# >=	        Greater than or equal to	    x >= y	
+# >=	        Greater than or equal tox >= y	
 # <=	        Less than or equal to	    x <= y	
