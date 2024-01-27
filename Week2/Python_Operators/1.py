85dc450a9d2b1d7ef6118435ff80b7268abe3442
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-27 16:41:11 +0600

    Week2 6Commit

---
+++ Week2/Python_Operators/1.py
@@ -0,0 +1,26 @@
+#Python Operators
+#Operators are used to perform operations on variables and values.
+#In the example below, we use the + operator to add together two values:
+print(123 + 567)
+
+
+#Python divides the operators in the following groups:
+#Arithmetic operators
+#Assignment operators
+#Comparison operators
+#Logical operators
+#Identity operators
+#Membership operators
+#Bitwise operators
+
+#Python Arithmetic Operators
+#Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+# Operator	Name	        Example	
+# +	        Addition	    x + y	
+# -	        Subtraction	    x - y	
+# *	        Multiplication	x * y	
+# /	        Division	    x / y	
+# %	        Modulus	        x % y	
+# **	    Exponentiation	x ** y	
+# //	    Floor division	x // y	
