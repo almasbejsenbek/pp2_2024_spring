b12cacfcfefaf1687698f6336e99e85075a33c98
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-27 16:04:46 +0600

    Week2 1Commit

---
+++ Week2/Python_Booleans/1.py
@@ -0,0 +1,7 @@
+print(1123 > 9)
+print(11233 == 11233)
+print(102123 < 2129)
+#In programming you often need to know if an expression is True or False.
+#You can evaluate any expression in Python, and get one of two answers, True or False.
+#When you compare two values, the expression is evaluated and Python returns the Boolean answer:
+
