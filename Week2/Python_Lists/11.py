cc5453e902f852730268c122bc49d14e2b3000ce
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-29 00:55:53 +0600

    Week2 21Commit

---
+++ Week2/Python_Lists/11.py
@@ -0,0 +1,27 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi" 
+print(fruits)
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+print(fruits)
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+print(fruits)
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:6])
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
