b2081f944ffba5d87f8554fc40646f475336f925
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-01-29 13:33:12 +0600

    Week2 31Commit

---
+++ Week2/Python_Sets/5.py
@@ -0,0 +1,19 @@
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+print(fruits)
+
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+print(fruits)
+
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+print(fruits)
