d5ffb8f496bd356ec5b38729124d36940ce6ee8b
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-02-04 21:50:04 +0600

    Week3 6commit

---
+++ Week3/Puthon_Functions/6.py
@@ -0,0 +1,15 @@
+def prime(number):
+    if int(number) < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+integer = input()
+numbers = integer.split()
+numbers = [int(num) for num in numbers]
+
+only_prime = list(filter(lambda x: prime(x), numbers))
+
+print("Prime numbers:", only_prime)
