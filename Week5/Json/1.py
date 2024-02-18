c74ed5607f10f55f88f249ce9d0298c2c62b8015
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-02-19 00:50:54 +0600

    Week5 commit15

--- Week5/Json/1.py
+++ Week5/Json/1.py
@@ -1,21 +1,25 @@
 import json
+
 with open("data.json", "r") as my_file:
     json_string = my_file.read()
+
 data = json.loads(json_string)
 
 interfaces = data.get('imdata', [])
+
+
 print("Interface Status")
 print("=" * 80)
 print("{:<50} {:<20} {:<8} {:<6}".format("DN", "Description", "Speed", "MTU"))
 print("-" * 50, "-" * 20, "-" * 8, "-" * 6)
 
-for i in interfaces:
-    l1physif = i.get('l1PhysIf', {})
-    att = l1physif.get('attributes', {})
+for interface in interfaces[1:]:
+    l1_phys_if = interface.get('l1PhysIf', {})
+    attributes = l1_phys_if.get('attributes', {})
     
-    dn = att.get('dn', '')
-    descr = att.get('descr', '')
-    speed = att.get('speed', 'inherit')
-    mtu = att.get('mtu', '')
+    dn = attributes.get('dn', '')
+    description = attributes.get('descr', '')
+    speed = attributes.get('speed', 'inherit')
+    mtu = attributes.get('mtu', '')
     
-    print("{:<50} {:<20} {:<8} {:<6}".format(dn, descr, speed, mtu))
+    print("{:<50} {:<20} {:<8} {:<6}".format(dn, description, speed, mtu))
