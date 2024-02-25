dcd82b68f076bb960607111700ef5aa93a84a68f
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-02-25 18:26:58 +0600

    Week6 commit11

---
+++ Week 6/Python RegEx/11.py
@@ -0,0 +1,14 @@
+import re
+import csv
+f = open("row.txt", "r", encoding="utf8")
+text = f.read()
+
+pattern = r"\n(?P<Order>[0-9]+)\.\n(?P<Name>.+)\n(?P<Count>.+)x(?P<Price>.+)\n(?P<Cost>.+)\n"
+
+results = re.finditer(pattern, text)
+
+with open('data.csv', 'w', newline='', encoding="utf8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Order', 'Name', 'Count', 'Price', 'Cost'])
+    for x in results:
+        writer.writerow([x.group('Order'), x.group('Name'), x.group('Count'), x.group('Price'), x.group('Cost')])
