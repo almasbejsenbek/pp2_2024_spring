e060ba39fc675d662e25b4b8fa629237d0df9e53
Almas Beisenbek <diasbejsenbek@MacBook-Air--Almas.local>
2024-02-26 12:02:54 +0600

    Week6 commit13

--- Week 6/Python RegEx/3.py
+++ Week 6/Python RegEx/3.py
@@ -1,5 +1,5 @@
 import re
 string = "almas_krasavchik mini_bmv Mercedes_En kushti"
-pattern = r"([a-z]+_[a-z])"
+pattern = r"([a-z]+_[a-z]+" ")"
 x = re.findall(pattern, string)
 print(x)
--- Week 6/Python RegEx/8.py
+++ Week 6/Python RegEx/8.py
@@ -1,5 +1,5 @@
-import re
-capitals = "CanberraBrasiliaOttawaBeijingParisBerlinNewdelhiJakartaRomeTokyoMexicoCityAmsterdamMoscowRiyadhSeoulAnkaraKievAbujaLagosLondon"
-f = r'[A-Z][a-z]*'
+
+f = rimport re
+capitals = "CanberraBrasiliaOttawaBeijingParisBerlinNewdelhiJakartaRomeTokyoMexicoCityAmsterdamMoscowRiyadhSeoulAnkaraKievAbujaLagosLondon"'[A-Z][a-z]*'
 x = re.findall(f, capitals)
 print(x)
